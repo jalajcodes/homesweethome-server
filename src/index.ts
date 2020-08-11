c09e392eee68ea5e2e63819d5756d62e878de194
Jalaj <Jalaj@mail.com>
2020-08-11 17:01:32 +0530

    added cookies ;)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express, { Application } from 'express';
+import cookieParser from 'cookie-parser';
 import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './graphql/index';
 import { connectDatabase } from './database';
@@ -11,8 +12,12 @@ const app = express();
 const mount = async (app: Application) => {
 	// Connect to db
 	const db = await connectDatabase();
+
+	// Use Cookie Parser Middleware
+	app.use(cookieParser(process.env.COOKIE_SECRET));
+
 	// Instantiate apollo server instance
-	const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ db }) });
+	const server = new ApolloServer({ typeDefs, resolvers, context: ({ req, res }) => ({ db, req, res }) });
 	// Use the middleware provided by apollol
 	server.applyMiddleware({ app, path: '/api' });
 
