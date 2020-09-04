aa63ca328e5909671a46176274bc04464eb8badb
codewithjalaj <jalaj@mail.com>
2020-09-04 12:06:30 +0530

    finished stripe connect and disconnect relovers

--- src/libs/api/Stripe.ts
+++ src/libs/api/Stripe.ts
@@ -1,7 +1,7 @@
 import stripe from 'stripe';
 
 const client = new stripe(`${process.env.S_SECRET_KEY}`, { apiVersion: '2020-08-27' });
-
+// this
 export const Stripe = {
 	connect: async (code: string) => {
 		const response = await client.oauth.token({
